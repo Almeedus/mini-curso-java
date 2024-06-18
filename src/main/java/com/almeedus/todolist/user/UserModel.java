4884ac41256eb3e607e6a4663ebc8d084b007acf
Eduardo Almeida <almeedusa@gmail.com>
2024-06-18 13:45:03 -0300

    Criando setters e getters para o user

--- src/main/java/com/almeedus/todolist/user/UserModel.java
+++ src/main/java/com/almeedus/todolist/user/UserModel.java
@@ -2,7 +2,34 @@ package com.almeedus.todolist.user;
 
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    //getters - buscar informações
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+
+    //setters - alterar informações
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
