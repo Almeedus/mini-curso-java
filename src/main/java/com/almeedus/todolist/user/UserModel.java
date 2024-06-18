640c5318c12f3cd043ed2a199892202c685ada2e
Eduardo Almeida <almeedusa@gmail.com>
2024-06-18 13:59:17 -0300

    Criando anotation no userModel para criar os setters e getters de forma automática

--- src/main/java/com/almeedus/todolist/user/UserModel.java
+++ src/main/java/com/almeedus/todolist/user/UserModel.java
@@ -1,35 +1,10 @@
 package com.almeedus.todolist.user;
 
-public class UserModel {
+import lombok.Data;
 
+@Data
+public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    //getters - buscar informações
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    
-
-    //setters - alterar informações
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
