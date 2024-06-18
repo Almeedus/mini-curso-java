da27c55a05879078ba8ce659391dd16da83518c2
Eduardo Almeida <almeedusa@gmail.com>
2024-06-18 12:10:54 -0300

    Primeiro Controller

---
+++ src/main/java/com/almeedus/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,16 @@
+package com.almeedus.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+public class MinhaPrimeiraController {
+
+    @GetMapping("/")
+    public String primeiraMensagem() {
+        return "Funcinou!";
+    }
+}
